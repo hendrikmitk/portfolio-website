38e51672981976e248f618dae43d49d6fe03dcaf
hendrikmitk <hharlichs@googlemail.com>
2021-02-04 10:56:02 +0100

    Implement background and basic nav

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # portfolio-website
-This is the code of my portfolio website. It's hand-made with ❤️ using [Sass](https://parceljs.org/) and [Parcel](https://parceljs.org/).
+This is the code of my portfolio website. It's hand-made with ❤️ using [Sass](https://parceljs.org/), [Jump.js](http://callmecavs.com/jump.js/) and [Parcel](https://parceljs.org/).
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,88 @@
-console.log('JS loaded');
+import jump from 'jump.js';
+
+/////////////
+// D A T A //
+/////////////
+
+const logo = document.querySelector('#navbar-logo');
+const about = document.querySelector('#navbar-about');
+const follow = document.querySelector('#navbar-follow');
+const contact = document.querySelector('#navbar-contact');
+
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+const textArray = [
+	'Node.js wizard',
+	'CSS artist',
+	'passionate Kitesurfer',
+	'JavaScript lover',
+	'HTML tinkerer',
+	'Photography enthusiast',
+];
+const typingDelay = 100;
+const erasingDelay = 60;
+const newTextDelay = 1200; // Time the fully typed text is displayed
+const initialOffset = 1400; // Offset before 1st start
+const newTextOffset = 400; // Offset before nth start
+let textArrayIndex = 0;
+let charIndex = 0;
+
+/////////////
+// H E L P //
+/////////////
+
+const jumpToSection = target => {
+	jump(target, {
+		duration: distance => Math.abs(distance) * 0.6,
+	});
+};
+
+const type = () => {
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		setTimeout(erase, newTextDelay);
+	}
+};
+
+const erase = () => {
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + newTextOffset);
+	}
+};
+
+/////////////
+// H E A R //
+/////////////
+
+document.addEventListener('DOMContentLoaded', () => {
+	if (textArray.length) setTimeout(type, initialOffset);
+});
+
+logo.addEventListener('click', () => {
+	jumpToSection('body');
+});
+
+about.addEventListener('click', () => {
+	jumpToSection('#about');
+});
+
+follow.addEventListener('click', () => {
+	jumpToSection('#follow');
+});
+
+contact.addEventListener('click', () => {
+	jumpToSection('#contact');
+});

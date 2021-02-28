9fccd388d95a839425933149da934ba07038a1c0
hendrikmitk <hharlichs@googlemail.com>
2021-02-28 18:44:54 +0100

    Include imprint

--- src/js/main.js
+++ src/js/main.js
@@ -18,17 +18,8 @@ hamburger.addEventListener('click', () => {
 // S E L E C T //
 /////////////////
 
-// Typing effect
-const typedTextSpan = document.querySelector('.typed-text');
-const cursorSpan = document.querySelector('.cursor');
-
 // IntersectionObserver
 const sections = document.querySelectorAll('section');
-
-// Date calculations
-const today = new Date();
-const ageSpan = document.querySelector('#age');
-const yearSpan = document.querySelector('#year');
 
 // Contact links
 const contactLinks = document.querySelectorAll('.to-contact');
@@ -40,24 +31,6 @@ const circleWrappers = document.querySelectorAll('.circle-wrapper');
 // D E F I N E //
 /////////////////
 
-// Typing effect
-const textArray = [
-	'Node.js wizard',
-	'CSS artist',
-	'passionate Kitesurfer',
-	'JavaScript lover',
-	'Crypto rascal',
-	'HTML tinkerer',
-	'Photography enthusiast',
-];
-const typingDelay = 100;
-const erasingDelay = 60;
-const newTextDelay = 1200; // Time the fully typed text is displayed
-const initialOffset = 1400; // Offset before 1st start
-const newTextOffset = 400; // Offset before nth start
-let textArrayIndex = 0;
-let charIndex = 0;
-
 // IntersectionObserver
 const options = {
 	rootMargin: '-40%',
@@ -65,39 +38,9 @@ const options = {
 let timeouts = {};
 const delay = 160;
 
-// Calculate age
-const dob = new Date('1988/03/19');
-
 /////////////////
 // H E L P E R //
 /////////////////
-
-// Typing effect
-const type = () => {
-	if (charIndex < textArray[textArrayIndex].length) {
-		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
-		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-		charIndex++;
-		setTimeout(type, typingDelay);
-	} else {
-		cursorSpan.classList.remove('typing');
-		setTimeout(erase, newTextDelay);
-	}
-};
-
-const erase = () => {
-	if (charIndex > 0) {
-		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
-		typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-		charIndex--;
-		setTimeout(erase, erasingDelay);
-	} else {
-		cursorSpan.classList.remove('typing');
-		textArrayIndex++;
-		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-		setTimeout(type, typingDelay + newTextOffset);
-	}
-};
 
 // IntersectionObserver
 const io = new IntersectionObserver(entries => {
@@ -139,13 +82,6 @@ const circleWatch = new IntersectionObserver(entries => {
 
 sections.forEach(section => circleWatch.observe(section));
 
-// Calculate age
-const calculateAge = () => {
-	const diff = today - dob;
-	const age = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
-	return age;
-};
-
 // Circular progress bars
 const drawCircles = () => {
 	circleWrappers.forEach(skill => {
@@ -170,26 +106,33 @@ const drawCircles = () => {
 // L I S T E N //
 /////////////////
 
-// Typing effect
-document.addEventListener('DOMContentLoaded', () => {
-	if (textArray.length) setTimeout(type, initialOffset);
-});
-
-// Calculate age
-document.addEventListener('DOMContentLoaded', () => {
-	ageSpan.textContent = calculateAge();
-	yearSpan.textContent = today.getFullYear();
-});
-
-// Smooth scrolling
+// Handle navigation clicks (Smooth scrolling)
 document.addEventListener('click', event => {
 	if (!event.target.matches('.nav-scroll')) return;
+
+	// Handle navigation clicks on `/imprint.html`
+	if (window.location.pathname == '/imprint.html') {
+		location.href = `/#${event.target.dataset.target}`;
+	}
+
 	event.preventDefault();
 	let element = document.getElementById(event.target.dataset.target);
 	element.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Contact links
+document.addEventListener('DOMContentLoaded', () => {
+	// Draw SVG child nodes when Skills are loaded
+	if (window.location.hash == '#skills') {
+		drawCircles();
+	}
+	// Add .active class to nav item when page is loaded
+	let hash = window.location.hash;
+	let hashName = hash.substring(1); // Remove hash sign
+	let navItem = document.getElementById(`navbar-${hashName}`);
+	navItem.classList.add('active');
+});
+
+// Handle Contact links
 contactLinks.forEach(link => {
 	link.addEventListener('click', () => {
 		let element = document.getElementById('contact');
---
+++ src/js/typewriter.js
@@ -0,0 +1,65 @@
+/////////////////
+// S E L E C T //
+/////////////////
+
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+/////////////////
+// D E F I N E //
+/////////////////
+
+const textArray = [
+	'Node.js wizard',
+	'CSS artist',
+	'passionate Kitesurfer',
+	'JavaScript lover',
+	'Crypto rascal',
+	'HTML tinkerer',
+	'Photography enthusiast',
+];
+const typingDelay = 100;
+const erasingDelay = 60;
+const newTextDelay = 1200; // Time the fully typed text is displayed
+const initialOffset = 1400; // Offset before 1st start
+const newTextOffset = 400; // Offset before nth start
+let textArrayIndex = 0;
+let charIndex = 0;
+
+/////////////////
+// H E L P E R //
+/////////////////
+
+const type = () => {
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		setTimeout(erase, newTextDelay);
+	}
+};
+
+const erase = () => {
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + newTextOffset);
+	}
+};
+
+/////////////////
+// L I S T E N //
+/////////////////
+
+document.addEventListener('DOMContentLoaded', () => {
+	if (textArray.length) setTimeout(type, initialOffset);
+});
